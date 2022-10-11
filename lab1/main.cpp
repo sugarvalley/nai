b4abd61b1e81b3ce8ea48c2db98fafb292223f3e
sugarvalley <cqrkowa@gmail.com>
2022-10-12 01:20:02 +0200

    dziala

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,11 +1,33 @@
-//
-// Created by s22202 on 05.10.2022.
-//
-
-#include "main.h"
 #include <iostream>
+#include <map>
+#include <functional>
+#include <vector>
+#include <string>
+#include <cmath>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+using mojafunkcja = function<float(int x, int y)>;
+
+void wypisz(const mojafunkcja& fun, int x, int y=0) {
+    cout << fun(x, y) << endl;
+}
+
+int main(int argc, char **argv) {
+    vector<string> argumenty(argv, argc + argv);
+    map<string, mojafunkcja> opcje;
+    opcje["add"] = [](int x, int y) { return x + y; };
+    opcje["mod"] = [](int x, int y) { return x % y; };
+    opcje["sin"] = [](int x, int y=0) { return sin(x); };
+        try {
+            wypisz(opcje[argumenty.at(1)], stoi(argumenty.at(2)), stoi(argumenty.at(3)));
+            } catch (out_of_range aor) {
+                if(argc == 3){
+                    wypisz(opcje[argumenty.at(1)], stoi(argumenty.at(2)));
+                } else {
+                    cout << "Wybierz opcje, dostepne to: " << endl;
+                    for (auto [k, v] : opcje) cout << " " << k;
+                    cout << endl;
+                }
+            }
     return 0;
 }
