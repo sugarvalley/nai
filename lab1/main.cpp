4a0bf496162830b46282601a403d8c4a2c8826e2
sugarvalley <cqrkowa@gmail.com>
2022-10-12 16:49:05 +0200

    lepszy catch

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -19,15 +19,20 @@ int main(int argc, char **argv) {
     opcje["mod"] = [](int x, int y) { return x % y; };
     opcje["sin"] = [](int x, int y=0) { return sin(x); };
         try {
-            wypisz(opcje[argumenty.at(1)], stoi(argumenty.at(2)), stoi(argumenty.at(3)));
+            if(argumenty.at(1) == "sin"){
+                wypisz(opcje[argumenty.at(1)], stoi(argumenty.at(2)));
+            } else if (argumenty.at(1) == "add" || argumenty.at(1) == "mod") {
+                wypisz(opcje[argumenty.at(1)], stoi(argumenty.at(2)), stoi(argumenty.at(3)));
+            } else {
+                cout << "Wybierz opcje, dostepne to: " << endl;
+                for (auto [k, v] : opcje) cout << " " << k;
+                cout << endl;
+                }
+
             } catch (out_of_range aor) {
-                if(argc == 3){
-                    wypisz(opcje[argumenty.at(1)], stoi(argumenty.at(2)));
-                } else {
                     cout << "Wybierz opcje, dostepne to: " << endl;
                     for (auto [k, v] : opcje) cout << " " << k;
                     cout << endl;
-                }
             }
     return 0;
 }
