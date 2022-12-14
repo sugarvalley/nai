a09a267df4724c830b6c1021669c505a0dad2f4e
sugarvalley <cqrkowa@gmail.com>
2022-12-14 16:27:28 +0100

    lab8

---
+++ cmake-build-debug/CMakeFiles/lab8.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab8.dir/lab8/lab8.cpp.obj"
+  "CMakeFiles/lab8.dir/lab8/lab8.cpp.obj.d"
+  "lab8.exe"
+  "lab8.exe.manifest"
+  "lab8.pdb"
+  "liblab8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
